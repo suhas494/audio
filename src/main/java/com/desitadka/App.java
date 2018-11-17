87fb8ace659d21231a39899a480625743e3d19e0
suhas494 <suhas.m494@gmail.com>
2018-11-17 12:28:29 +0530

    files added

---
+++ src/main/java/com/desitadka/App.java
@@ -0,0 +1,13 @@
+package com.desitadka;
+
+/**
+ * Hello world!
+ *
+ */
+public class 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Desi Tadka!" );
+    }
+}
